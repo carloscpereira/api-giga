1c36aac05911b69b3b2d2dfb6f4fd31e876190e2
Saulo Lins <saulo.linsd@gmail.com>
2020-07-15 10:42:27 -0300

    "feat(Sentry): Error handling
    Using Sentry to handle errors within the application"

--- src/app.js
+++ src/app.js
@@ -1,21 +1,42 @@
-import express from 'express';
-import routes from './routes';
+import 'dotenv/config';
 
+import express from 'express';
+import * as Sentry from '@sentry/node';
+import Youch from 'youch';
+
+import 'express-async-errors';
+
+import routes from './routes';
+import sentryConfig from './config/sentry';
 import './database';
 
 class App {
   constructor() {
     this.server = express();
+
+    Sentry.init(sentryConfig);
+
     this.middleware();
     this.routes();
+    this.exceptionHandler();
   }
 
   middleware() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
   }
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+
+      return res.status(500).json(errors);
+    });
   }
 }
 
