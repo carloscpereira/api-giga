be9b841ab7d6c1c6016878a6a0f00adf66e09512
Saulo Lins <saulo.linsd@gmail.com>
2020-08-07 09:52:18 -0300

    feat(exceptionHandler): Return standardization

--- src/app.js
+++ src/app.js
@@ -79,7 +79,12 @@ class App {
     this.server.use(async (err, req, res, next) => {
       const errors = await new Youch(err, req).toJSON();
 
-      return res.status(500).json(errors);
+      if (process.env.NODE_ENV !== 'production')
+        return res.status(500).json(errors);
+
+      return res
+        .status(500)
+        .json({ error: 500, data: { message: 'Internal Server Error' } });
     });
   }
 }
