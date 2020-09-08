87a0705571ed7dbdd95d4b3293eb387df63c7f26
Saulo Lins <saulo.linsd@gmail.com>
2020-09-08 16:42:19 -0300

    chore(App): Strip app class Routes

--- src/app.js
+++ src/app.js
@@ -12,26 +12,16 @@ import swaggerUi from 'swagger-ui-express';
 
 import 'express-async-errors';
 
-import { checkAuthorization } from './app/middlewares';
+// import { checkAuthorization } from './app/middlewares';
 
-import {
-  parcelaRouter,
-  loteRouter,
-  ocorrenciaRouter,
-  logCartaoCreditoRouter,
-  contratoRouter,
-  modalidadePagamentoRouter,
-  tipoCarteiraRouter,
-} from './routes';
+import routes from './routes';
 
 import sentryConfig from './config/sentry';
 import corsConfig from './config/cors';
 
 const YAML = require('yamljs');
 
-const swaggerDocument = YAML.load(
-  resolve(__dirname, '..', 'swagger', 'swagger.yaml')
-);
+const swaggerDocument = YAML.load(resolve(__dirname, '..', 'swagger', 'swagger.yaml'));
 // import './database';
 
 class App {
@@ -59,7 +49,7 @@ class App {
     this.server.use(cors(corsConfig));
     this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
-    this.server.use(checkAuthorization);
+    // this.server.use(checkAuthorization);
     this.server.use(
       queryParams({
         format: 'sql',
@@ -69,14 +59,8 @@ class App {
   }
 
   routes() {
-    this.server.use('/parcelas/log/cartao-credito', logCartaoCreditoRouter);
-    this.server.use('/ocorrencias', ocorrenciaRouter);
-    this.server.use('/lotes', loteRouter);
-    this.server.use('/parcelas', parcelaRouter);
-    this.server.use('/contratos', contratoRouter);
-    this.server.use('/modalidade-pagamentos', modalidadePagamentoRouter);
-    this.server.use('/tipo-carteira', tipoCarteiraRouter);
     this.server.use(Sentry.Handlers.errorHandler());
+    this.server.use(routes);
   }
 
   exceptionHandler() {
@@ -84,12 +68,9 @@ class App {
       console.log(err);
       const errors = await new Youch(err, req).toJSON();
 
-      if (process.env.NODE_ENV !== 'production')
-        return res.status(500).json(errors);
+      if (process.env.NODE_ENV !== 'production') return res.status(500).json(errors);
 
-      return res
-        .status(500)
-        .json({ error: 500, data: { message: 'Internal Server Error' } });
+      return res.status(500).json({ error: 500, data: { message: 'Internal Server Error' } });
     });
   }
 }
