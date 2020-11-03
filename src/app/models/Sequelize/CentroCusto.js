243df91c4fe6397ed95d0bf29212c30574c487f8
Saulo Lins <saulo.linsd@gmail.com>
2020-11-03 09:48:20 -0300

    feat(Cento Custo): Adding the CentroCusto scheme

---
+++ src/app/models/Sequelize/CentroCusto.js
@@ -0,0 +1,52 @@
+/* eslint-disable no-param-reassign */
+import Sequelize, { Model } from 'sequelize';
+
+export default class CentroCusto extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        descricao: Sequelize.STRING,
+        quantidadefuncionario: Sequelize.INTEGER,
+        numerometroquadrado: Sequelize.DOUBLE,
+        numerofator: Sequelize.DOUBLE,
+        departamentoid: Sequelize.BIGINT,
+        setorid: Sequelize.BIGINT,
+        empresaid: Sequelize.BIGINT,
+        nomefantasiacarteira: Sequelize.STRING,
+        localid: Sequelize.BIGINT,
+        assuntoid: Sequelize.BIGINT,
+        search: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            const departamento = this.departamento ? this.departamento.descricao.trim() : '';
+            const setor = this.setor ? this.setor.descricao.trim() : '';
+            const razaoSocial = this.empresa ? this.empresa.razaosocial.trim() : '';
+            const nomeFantasia = this.empresa ? this.empresa.nomefantasia.trim() : '';
+
+            return `${razaoSocial || ''} (${nomeFantasia || ''}) - ${departamento || ''} - ${setor || ''}`;
+          },
+        },
+      },
+      { sequelize, tableName: 'centrocusto' }
+    );
+
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.PessoaJuridica, {
+      foreignKey: 'empresaid',
+      as: 'empresa',
+    });
+
+    this.belongsTo(models.Setor, {
+      foreignKey: 'setorid',
+      as: 'setor',
+    });
+
+    this.belongsTo(models.Departamento, {
+      foreignKey: 'departamentoid',
+      as: 'departamento',
+    });
+  }
+}
