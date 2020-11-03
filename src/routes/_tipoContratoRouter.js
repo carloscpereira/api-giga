bc238f7271a8e04f330dc3909ab78992491d82ac
Saulo Lins <saulo.linsd@gmail.com>
2020-11-03 09:30:29 -0300

    feat(Tipo Contrato): Adding new features to the api

---
+++ src/routes/_tipoContratoRouter.js
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+
+import TipoContrato from '../app/controllers/tipoContratoController';
+
+import { checkOperator } from '../app/middlewares';
+
+const agenciaRouter = Router();
+
+agenciaRouter.get('/:operator/', checkOperator, TipoContrato.index);
+// agenciaRouter.post('/:operator/', checkOperator, TipoContrato.create);
+agenciaRouter.get('/:operator/:id', checkOperator, TipoContrato.show);
+// agenciaRouter.put('/:operator/:id', checkOperator, TipoContrato.update);
+// agenciaRouter.delete('/:operator/:id', checkOperator, TipoContrato.destroy);
+
+export default agenciaRouter;
