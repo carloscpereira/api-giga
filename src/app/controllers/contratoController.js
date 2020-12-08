566d81c931f140bd66c6f381e3ef2725940fab3f
Saulo Lins <saulo.linsd@gmail.com>
2020-12-08 16:47:52 -0300

    fix(Contratos/Controller): Correção de Crash do router de Contratos

--- src/app/controllers/contratoController.js
+++ src/app/controllers/contratoController.js
@@ -1,8 +1,6 @@
 import * as Yup from 'yup';
 import queryStringConverter from 'sequelize-querystring-converter';
-import { Op } from 'sequelize';
 import Contrato from '../models/Sequelize/Contrato';
-import Pessoa from '../models/Sequelize/Pessoa';
 import TipoContrato from '../models/Sequelize/TipoContrato';
 import PessoaJuridica from '../models/Sequelize/PessoaJuridica';
 import TipoTabelaUso from '../models/Sequelize/TipoTabelaUso';
@@ -15,6 +13,10 @@ import Titulo from '../models/Sequelize/Titulo';
 import TipoCarteira from '../models/Sequelize/TipoCarteira';
 
 import CriarContratoService from '../services/CriaContratoService';
+import MigrarContratoService from '../services/MigrarContratoService';
+
+import RemoveMembroContratoService from '../services/RemoveMembroContratoService';
+import AdicionarMembroContratoService from '../services/AdicionarMembroContratoService';
 
 class ContratoController {
   async index(req, res) {
@@ -98,15 +100,10 @@ class ContratoController {
   async show(req, res) {
     const { id } = req.params;
 
-    const contrato = await Contrato.findAll({
-      where: {
-        [Op.or]: {
-          id,
-        },
-      },
+    const contrato = await Contrato.findByPk(id, {
       include: [
-        { model: Pessoa, as: 'responsavelpf', where: { cpf: id } },
-        { model: Pessoa, as: 'responsavelpj', where: { cnpj: id } },
+        { model: PessoaFisica, as: 'responsavel_pessoafisica' },
+        { model: PessoaJuridica, as: 'responsavel_pessoajuridica' },
       ],
     });
 
@@ -126,6 +123,18 @@ class ContratoController {
     });
 
     return res.json({ error: null, data: responseContrato });
+  }
+
+  async migrar(req, res) {
+    console.log(req.formValidation);
+
+    const contrato = await MigrarContratoService.execute({
+      id_contrato: req.params.id,
+      request: req.body,
+      sequelize: req.sequelize,
+    });
+
+    return res.json(contrato);
   }
 
   async update(req, res) {
@@ -211,6 +220,144 @@ class ContratoController {
     const contrato = await Contrato.update(data, {
       where: { id },
       returning: true,
+    });
+
+    return res.json({ error: null, data: contrato });
+  }
+
+  async adicionarAssociado(req, res) {
+    const {
+      body: { GrupoFamiliar: id_grupofamiliar, Vinculo: vinculo, ...beneficiario },
+      params: { id: id_contrato },
+      sequelize,
+    } = req;
+
+    await AdicionarMembroContratoService.execute({
+      beneficiario,
+      id_contrato,
+      id_grupofamiliar,
+      sequelize,
+      vinculo,
+    });
+
+    const contrato = await Contrato.findByPk(id_contrato, {
+      include: [
+        { model: PessoaFisica, as: 'responsavel_pessoafisica' },
+        { model: PessoaJuridica, as: 'responsavel_pessoajuridica' },
+        {
+          model: GrupoFamiliar,
+          as: 'gruposfamiliar',
+          include: [
+            {
+              model: PessoaFisica,
+              as: 'beneficiarios',
+              attributes: { exclude: ['alteracao', 'secretariaid', 'usuario', 'status', 'tipodadosid'] },
+              through: {
+                as: 'dados',
+                attributes: [
+                  'dataadesao',
+                  'ativo',
+                  'datadesativacao',
+                  'tipobeneficiarioid',
+                  'versaoplanoid',
+                  'planoid',
+                  'tipobeneficiarioid',
+                  'tipocarteiraid',
+                  'valor',
+                  'descontovalor',
+                  'descontoporcent',
+                ],
+              },
+            },
+
+            {
+              model: PessoaFisica,
+              as: 'responsaveis',
+              attributes: { exclude: ['alteracao', 'secretariaid', 'usuario', 'status', 'tipodadosid'] },
+              through: {
+                as: 'dados',
+                attributes: [
+                  'dataadesao',
+                  'ativo',
+                  'datadesativacao',
+                  'tipobeneficiarioid',
+                  'versaoplanoid',
+                  'planoid',
+                  'tipobeneficiarioid',
+                  'tipocarteiraid',
+                  'valor',
+                  'descontovalor',
+                  'descontoporcent',
+                ],
+              },
+            },
+          ],
+        },
+      ],
+    });
+
+    return res.json({ error: null, data: contrato });
+  }
+
+  async removerAssociado(req, res) {
+    const { id: id_contrato, beneficiarioid: id_beneficiario } = req.params;
+
+    await RemoveMembroContratoService.execute({ sequelize: req.sequelize, id_beneficiario, id_contrato });
+
+    const contrato = await Contrato.findByPk(id_contrato, {
+      include: [
+        { model: PessoaFisica, as: 'responsavel_pessoafisica' },
+        { model: PessoaJuridica, as: 'responsavel_pessoajuridica' },
+        {
+          model: GrupoFamiliar,
+          as: 'gruposfamiliar',
+          include: [
+            {
+              model: PessoaFisica,
+              as: 'beneficiarios',
+              attributes: { exclude: ['alteracao', 'secretariaid', 'usuario', 'status', 'tipodadosid'] },
+              through: {
+                as: 'dados',
+                attributes: [
+                  'dataadesao',
+                  'ativo',
+                  'datadesativacao',
+                  'tipobeneficiarioid',
+                  'versaoplanoid',
+                  'planoid',
+                  'tipobeneficiarioid',
+                  'tipocarteiraid',
+                  'valor',
+                  'descontovalor',
+                  'descontoporcent',
+                ],
+              },
+            },
+
+            {
+              model: PessoaFisica,
+              as: 'responsaveis',
+              attributes: { exclude: ['alteracao', 'secretariaid', 'usuario', 'status', 'tipodadosid'] },
+              through: {
+                as: 'dados',
+                attributes: [
+                  'dataadesao',
+                  'ativo',
+                  'datadesativacao',
+                  'tipobeneficiarioid',
+                  'versaoplanoid',
+                  'planoid',
+                  'tipobeneficiarioid',
+                  'tipocarteiraid',
+                  'valor',
+                  'descontovalor',
+                  'descontoporcent',
+                ],
+              },
+            },
+          ],
+        },
+      ],
     });
 
     return res.json({ error: null, data: contrato });
