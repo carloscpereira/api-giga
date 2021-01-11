2d653a9f2933c2fd4e6d244dc913f0c6807efe3f
Saulo Lins <saulo.linsd@gmail.com>
2021-01-11 12:48:05 -0300

    Fast Alte

--- src/app/controllers/colaboradoresClinicasController.js
+++ src/app/controllers/colaboradoresClinicasController.js
@@ -13,7 +13,8 @@ class ColaboradoresClinicaController {
         SELECT DISTINCT ON (nome) nome,
           spd.id,
           cpf,
-          cro            as "cro"
+          cro            as "cro",
+          qualificacao   as "qualificacao"
         FROM cn_dentista_clinica cdc
         INNER JOIN sp_dadospessoafisica spd ON cdc.dentistapfid = spd.id
         LEFT JOIN LATERAL (SELECT pav.dadocampo as cro
@@ -23,6 +24,13 @@ class ColaboradoresClinicaController {
                     AND pav.campo = 1
                     AND pav.dadocampo IS NOT NULL
                   LIMIT 1) cro ON TRUE
+        LEFT JOIN LATERAL (SELECT pav.dadocampo as qualificacao
+                    FROM sp_pessoaatributovinculo pav
+                    WHERE pav.pessoaid = cdc.dentistapfid
+                      AND pav.vinculoid = ${operator === 'atemde' ? '60' : '63'}
+                      AND pav.campo = 7
+                      AND pav.dadocampo IS NOT NULL
+                    LIMIT 1) qualificacao ON TRUE
         WHERE cdc.numerocontrato = :idcontrato;
       `,
         { type: QueryTypes.SELECT, replacements: { idcontrato } }
@@ -30,6 +38,7 @@ class ColaboradoresClinicaController {
 
       return res.json(colaboradores);
     } catch (error) {
+      console.log(error);
       return res.status(500).json({ error: 500, data: { message: 'Internal server error' } });
     }
   }
