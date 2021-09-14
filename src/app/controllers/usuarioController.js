6f24bc9f2f434cc06166008a74f7128631d3f9ff
Carlos Cesar <carlos.csp@icloud.com>
2021-09-14 14:47:19 -0300

    fix controller usuarios

--- src/app/controllers/usuarioController.js
+++ src/app/controllers/usuarioController.js
@@ -30,31 +30,46 @@ class UsuarioController {
 
         /** Testa se já existe o usuario */
         const query = `SELECT  usr_codigo, usr_login, usr_senha, usr_nome, usr_email, pessoaid
-        FROM sc_portal_associado.fr_usuario WHERE usr_login =${usr_login}`;
+        FROM sc_portal_associado.fr_usuario WHERE usr_login ='${usr_login}'`;
 
         const resQuery = await sequelize.query(query, { type: QueryTypes.SELECT });
 
-        if (!resQuery) {
+        if (resQuery.length != 0) {
             return res.json({ error: 400, message: 'Usuário já cadastrado.' });
         }
 
         /**find pessoaID */
         const queryFindPessoa = `SELECT id FROM sp_dadospessoafisica WHERE cpf = '${usr_login}'`;
-        const pessoaID = await sequelize.query(queryFindPessoa, { type: QueryTypes.SELECT });
+        const { id: pessoaID } = await sequelize.query(queryFindPessoa, { type: QueryTypes.SELECT });
+
+        if (!pessoaID) {
+            return res.json({ error: 400, message: 'Usuário sem cadastro na operadora.' });
+        }
+
+        const queryContrato = `SELECT id FROM cn_associadopf
+                              WHERE responsavelfinanceiroid = ${pessoaID}
+                              UNION
+                              SELECT contratoid FROM cn_beneficiario
+                              WHERE pessoabeneficiarioid = ${pessoaID}`;
+
+        const contrato = await sequelize.query(queryContrato, { type: QueryTypes.SELECT });
+        if (contrato.length != 0) {
+            return res.json({ error: 400, message: 'Usuário não possue contrato na operadora.' });
+        }
 
         const insertUsuario = `INSERT INTO sc_portal_associado.fr_usuario (
           usr_login, usr_senha, usr_nome, usr_email, pessoaid) VALUES (
             '${usr_login}', '${usr_senha}', '${usr_nome}', '${usr_email}', ${pessoaID})
             RETURNING usr_codigo, usr_login, usr_senha, usr_nome, usr_email, pessoaid`;
 
-        const newUsuario = await sequelize.query(insertUsuario);
+        const newUsuario = await sequelize.query(insertUsuario, { type: QueryTypes.INSERT });
 
         const queryCriptSenha = `UPDATE sc_portal_associado.fr_usuario
         SET usr_senha = 'md5(${newUsuario.usr_codigo}||${newUsuario.usr_senha})'
         WHERE usr_codigo = ${newUsuario.usr_codigo}
         RETURNING usr_codigo, usr_login, usr_senha, usr_nome, usr_email, pessoaid`;
 
-        const resultUsuario = await sequelize.query(queryCriptSenha);
+        const resultUsuario = await sequelize.query(queryCriptSenha, { type: QueryTypes.UPDATE });
 
         return res.json({ error: null, data: resultUsuario });
     }
