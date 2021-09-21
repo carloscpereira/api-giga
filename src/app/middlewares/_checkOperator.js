93e58c2392770a19d2fbf1a93f1655ae32702c69
Carlos Cesar <carlos.csp@icloud.com>
2021-09-21 20:23:02 -0300

    fix middleware para homolog

--- src/app/middlewares/_checkOperator.js
+++ src/app/middlewares/_checkOperator.js
@@ -4,37 +4,37 @@ import knex from '../../utils/Knex';
 import connectionConfig from '../../config/database';
 import Database from '../../database';
 
-export default async (req, res, next) => {
-  const schema = Yup.object().shape({
-    operator: Yup.string()
-      .lowercase()
-      .matches(/(atemde|idental)/)
-      .required(),
-  });
+export default async(req, res, next) => {
+    const schema = Yup.object().shape({
+        operator: Yup.string()
+            .lowercase()
+            .matches(/(atemde|idental|homolog)/)
+            .required(),
+    });
 
-  if (!(await schema.isValid(req.params))) {
-    try {
-      await schema.validate(req.params);
-    } catch (err) {
-      return res.status(401).json({
-        error: 401,
-        data: {
-          message: 'Operator is required, and must be between atemde e idental',
-          errors: err.errors,
-        },
-      });
+    if (!(await schema.isValid(req.params))) {
+        try {
+            await schema.validate(req.params);
+        } catch (err) {
+            return res.status(401).json({
+                error: 401,
+                data: {
+                    message: 'Operator is required, and must be between atemde e idental',
+                    errors: err.errors,
+                },
+            });
+        }
     }
-  }
 
-  const { operator } = req.params;
+    const { operator } = req.params;
 
-  req.connection = connectionConfig[process.env.NODE_ENV].databases[operator];
+    req.connection = connectionConfig[process.env.NODE_ENV].databases[operator];
 
-  // eslint-disable-next-line no-new
-  req.sequelize = new Database(req.connection).connection;
+    // eslint-disable-next-line no-new
+    req.sequelize = new Database(req.connection).connection;
 
-  req.pool = pool[operator];
-  req.knex = knex[operator];
+    req.pool = pool[operator];
+    req.knex = knex[operator];
 
-  return next();
+    return next();
 };
